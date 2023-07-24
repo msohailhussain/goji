a5b2d83dbd27ca1e269b065c36ea6153b1238735
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-07-24 15:27:27 +0200

    .

---
+++ math/diophantine/linear.go
@@ -0,0 +1,33 @@
+package math
+
+import "golang.org/x/exp/constraints"
+
+// Assumptions:
+// - At least one != 0
+// source: https://cp-algorithms.com/algebra/extended-euclid-algorithm.html#algorithm
+func ExtendedEuclideanAlgorithm[T constraints.Integer](a, b T) (T, int, int) {
+	if b == 0 {
+		return a, 1, 0
+	}
+	gcd, xTmp, yTmp := ExtendedEuclideanAlgorithm(b, a%b)
+	y := xTmp - yTmp*int(a/b)
+	x := yTmp
+	return gcd, x, y
+}
+
+// https://cp-algorithms.com/algebra/linear-diophantine-equation.html#algorithmic-solution
+// Given ax+by=c returns:
+// - if it has solutions (zero or infinite)
+// - gcd(a,b)
+// - one solution
+func ComputeLinearDiophantine[T constraints.Integer](a, b, c T) (hasSolutions bool, gcd T, x, y int) {
+	g, x, y := ExtendedEuclideanAlgorithm(a, b)
+	if c%g != 0 {
+		return false, 0, 0, 0
+	}
+
+	factor := int(c / g)
+	x *= factor
+	y *= factor
+	return true, g, x, y
+}
--- math/discrete.go
+++ math/discrete.go
@@ -2,33 +2,21 @@ package math
 
 import "golang.org/x/exp/constraints"
 
+// Euclidean algorithm
+// It returns the positive solution
 // Assumptions:
-// - Both >= 0 and
 // - At least one != 0
 func GCD[T constraints.Integer](a, b T) T {
 	if b == 0 {
-		return a
+		return Abs(a)
 	}
 	return GCD(b, a%b)
 }
 
+// It returns the positive solution
+// If one is 0, returns 0
 // Assumptions:
-// - Both >= 0 and
 // - At least one != 0
-func LCM[T constraints.Unsigned](a, b T) T {
-	return (a * b) / GCD(a, b)
-}
-
-// Assumptions:
-// - Both >= 0 and
-// - At least one != 0
-// source: https://cp-algorithms.com/algebra/linear-diophantine-equation.html#algorithmic-solution
-func ExtendedGCD[T constraints.Integer](a, b T) (T, int, int) {
-	if b == 0 {
-		return a, 1, 0
-	}
-	gcd, xTmp, yTmp := ExtendedGCD(b, a%b)
-	y := xTmp - yTmp*int(a/b)
-	x := yTmp
-	return gcd, x, y
+func LCM[T constraints.Integer](a, b T) T {
+	return Abs(a*b) / GCD(a, b)
 }
