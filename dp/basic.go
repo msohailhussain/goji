5fb796b46491e20cdec371cbf42434478b775c0d
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-07-26 22:11:08 +0200

    .

--- dp/basic.go
+++ dp/basic.go
@@ -1,21 +1,23 @@
 package dp
 
-func NewDP[K comparable, V any](computeValue func(dp *DP[K, V], key K) (value V)) *DP[K, V] {
+// Top-down or memoization approach to dp
+type DP[K comparable, V any] struct {
+	m            map[K]V
+	computeValue func(get func(key K) (value V), key K) (value V)
+}
+
+func NewDP[K comparable, V any](computeValue func(get func(key K) (value V), key K) (value V)) *DP[K, V] {
 	return &DP[K, V]{
 		m:            make(map[K]V),
 		computeValue: computeValue,
 	}
 }
 
-type DP[K comparable, V any] struct {
-	m            map[K]V
-	computeValue func(dp *DP[K, V], key K) (value V)
-}
-
 func (dp *DP[K, V]) Get(key K) (value V) {
 	value, found := dp.m[key]
 	if !found {
-		value = dp.computeValue(dp, key)
+		value = dp.computeValue(dp.Get, key)
+		dp.m[key] = value
 	}
 	return
 }
