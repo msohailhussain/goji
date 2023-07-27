968e15f4d9d400b3a570c1f23011b25cd5006473
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-07-27 12:57:32 +0200

    .

--- dp/basic.go
+++ dp/basic.go
@@ -1,16 +1,20 @@
 package dp
 
+import "fmt"
+
 // Top-down or memoization approach to dp
 //
 // Usage example for fiboacci:
 // dp := NewDP(
-// 	func(get func(int) int,
-// 		k int) int {
-// 		if k <= 1{
-// 			return k
-// 		}
-// 		return get(k-1)+get(k-2)
-// 	},
+//
+//	func(get func(int) int,
+//		k int) int {
+//		if k <= 1{
+//			return k
+//		}
+//		return get(k-1)+get(k-2)
+//	},
+//
 // )
 // dp.Get(20)
 type DP[Key comparable, Value any] struct {
@@ -25,11 +29,21 @@ func NewDP[Key comparable, Value any](recurrence func(get func(Key) Value, k Key
 	}
 }
 
-func (dp *DP[Key, Value]) Get(key Key) Value {
-	value, found := dp.m[key]
+func (dp *DP[Key, Value]) Get(k Key) Value {
+	value, found := dp.m[k]
 	if !found {
-		value = dp.recurrence(dp.Get, key)
-		dp.m[key] = value
+		value = dp.recurrence(dp.Get, k)
+		dp.m[k] = value
 	}
 	return value
+}
+func (dp *DP[Key, Value]) Set(k Key, v Value) {
+	dp.m[k] = v
+}
+func (dp *DP[Key, Value]) String() string {
+	s := ""
+	for k, v := range dp.m {
+		s += fmt.Sprint(k) + " -> " + fmt.Sprintln(v)
+	}
+	return s
 }
