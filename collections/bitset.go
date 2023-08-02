6afcd2d2bfba0db557f8e1cc3da5bd3cf6c54209
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-08-03 00:18:00 +0200

    .

--- collections/bitset.go
+++ collections/bitset.go
@@ -38,7 +38,7 @@ func (b *Bitset) ShiftLeft(c int) {
 	c %= blockSize
 
 	var carry uint = 0
-	for i := len(b.data)-1; i>= 0; i-- {
+	for i := len(b.data) - 1; i >= 0; i-- {
 		carry, b.data[i] = b.data[i]>>(blockSize-c), carry|(b.data[i]<<c)
 	}
 
@@ -66,7 +66,40 @@ func (b *Bitset) ShiftRight(c int) {
 	}
 	b.data[len(b.data)-1] &= math.MaxUint << (len(b.data)*blockSize - b.size)
 }
+func (b *Bitset) And(other *Bitset) {
+	for i := range b.data {
+		b.data[i] &= other.data[i]
+	}
+}
+func (b *Bitset) Or(other *Bitset) {
+	for i := range b.data {
+		b.data[i] |= other.data[i]
+	}
+}
+func (b *Bitset) Xor(other *Bitset) {
+	for i := range b.data {
+		b.data[i] ^= other.data[i]
+	}
+}
+func (b *Bitset) Not(other *Bitset) {
+	for i := range b.data {
+		b.data[i] = ^other.data[i]
+	}
+}
+func (b *Bitset) AndNot(other *Bitset) {
+	for i := range b.data {
+		b.data[i] &^= other.data[i]
+	}
+}
+func (b *Bitset) Clone() *Bitset {
+	dataCopy := make([]uint, len(b.data))
+	copy(dataCopy, b.data)
 
+	return &Bitset{
+		size: b.size,
+		data: dataCopy,
+	}
+}
 func (b *Bitset) Set(index int, value bool) {
 	elem := &b.data[index/blockSize]
 	index = index % blockSize
