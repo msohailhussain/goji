132c95036973cc31829b55bd3fe79ad2eb3c6dbf
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-12-06 20:35:37 +0000

    .

--- collections/graph/string.go
+++ collections/graph/string.go
@@ -8,44 +8,46 @@ import (
 	"github.com/lorenzotinfena/goji/utils/constraints"
 )
 
+// Using code from: github.com/hediet/vscode-debug-visualizer/blob/master/demos/golang/demo.go
+
+type nodeGraphDataDebug struct {
+	ID    string `json:"id"`
+	Label string `json:"label,omitempty"`
+	Color string `json:"color,omitempty"`
+	Shape string `json:"shape,omitempty"`
+}
+
+type edgeGraphDataDebug struct {
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Label  string `json:"label,omitempty"`
+	ID     string `json:"id"`
+	Color  string `json:"color,omitempty"`
+	Dashes bool   `json:"dashes,omitempty"`
+}
+
+type graphDebug struct {
+	Kind  map[string]bool      `json:"kind"`
+	Nodes []nodeGraphDataDebug `json:"nodes"`
+	Edges []edgeGraphDataDebug `json:"edges"`
+}
+
 func toString[V comparable, W constraints.Integer | constraints.Float, G Graph[V, W]](g G) string {
-	type NodeGraphData struct {
-		ID    string `json:"id"`
-		Label string `json:"label,omitempty"`
-		Color string `json:"color,omitempty"`
-		Shape string `json:"shape,omitempty"`
-	}
-
-	type EdgeGraphData struct {
-		From   string `json:"from"`
-		To     string `json:"to"`
-		Label  string `json:"label,omitempty"`
-		ID     string `json:"id"`
-		Color  string `json:"color,omitempty"`
-		Dashes bool   `json:"dashes,omitempty"`
-	}
-
-	type Graph struct {
-		Kind  map[string]bool `json:"kind"`
-		Nodes []NodeGraphData `json:"nodes"`
-		Edges []EdgeGraphData `json:"edges"`
-	}
-
-	graph := &Graph{
+	graph := &graphDebug{
 		Kind:  map[string]bool{"graph": true},
-		Nodes: []NodeGraphData{},
-		Edges: []EdgeGraphData{},
+		Nodes: []nodeGraphDataDebug{},
+		Edges: []edgeGraphDataDebug{},
 	}
 	for _, v := range g.Vertices() {
 		s := fmt.Sprint(v)
-		graph.Nodes = append(graph.Nodes, NodeGraphData{ID: s, Label: s})
+		graph.Nodes = append(graph.Nodes, nodeGraphDataDebug{ID: s, Label: s})
 	}
 
 	for _, v := range g.Vertices() {
 		for _, adj := range g.GetAdjacents(v) {
 			s1 := fmt.Sprint(v)
 			s2 := fmt.Sprint(adj)
-			tmp := EdgeGraphData{From: s1, To: s2}
+			tmp := edgeGraphDataDebug{From: s1, To: s2}
 			gra, isWeighted := any(g).(WeightedGraph[V, W])
 			if isWeighted {
 				tmp.Label = fmt.Sprint(gra.GetWeight(v, adj))
--- collections/graph/string_test.go
+++ collections/graph/string_test.go
@@ -2,10 +2,45 @@
 package graph_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/lorenzotinfena/goji/collections/graph"
 )
+
+func TestGraphDFS(t *testing.T) {
+	g := graph.NewUnitGraph[int]()
+	for i := 0; i < 10; i++ {
+		g.AddVertex(i)
+	}
+	g.AddEdge(0, 3)
+	g.AddEdge(0, 4)
+	g.AddEdge(0, 5)
+	g.AddEdge(1, 6)
+	g.AddEdge(1, 8)
+	g.AddEdge(8, 9)
+	g.AddEdge(9, 8)
+	g.AddEdge(3, 7)
+	g.AddEdge(7, 3)
+	g.AddEdge(5, 4)
+	g.AddEdge(7, 6)
+	g.AddEdge(1, 9)
+	g.AddEdge(2, 1)
+	g.AddEdge(3, 2)
+	g.AddEdge(4, 5)
+	g.AddEdge(5, 3)
+	g.AddEdge(7, 2)
+	g.AddEdge(5, 5)
+	g.AddEdge(9, 9)
+	s := g.String()
+	_ = s
+	it := g.GetIteratorDFS(0)
+	it2 := g.GetIteratorDFS(0)
+	for it.HasNext() {
+		fmt.Println(it.Next())
+		fmt.Println(it2.Next())
+	}
+}
 
 func TestGraphDrawing(t *testing.T) {
 	g := graph.NewUnitGraph[int]()
